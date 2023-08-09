fdcbb47640b0be0d171880d3a4da4011dd305eba
Nastya Bukshtunovich <nastya.trader@gmail.com>
2023-08-10 01:05:40 +0300

    added sass files

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+
+  
+  /*Фиксированный футер*/
+
+    // console.log(document.querySelector(".main-nav").getBoundingClientRect().height );
+
+    // const { heigh: headerHeight } = document
+    // .querySelector(".main-nav")
+    // .getBoundingClientRect();
+    // document.body.style.paddingTop = `${mainNavHeight}px`;
